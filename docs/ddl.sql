46530b9030634988e6808aa07d11f4806ec333c0
Felipe de Sousa <felipescoelho517@gmail.com>
2023-11-25 15:12:08 -0300

    Mudança em alguns pontos do ddl.sql

--- docs/ddl.sql
+++ docs/ddl.sql
@@ -1,6 +1,7 @@
 BEGIN TRANSACTION;
+
 CREATE TABLE Treinador (
-    treinador_id int PRIMARY KEY NOT NULL,
+    treinador_id serial PRIMARY KEY NOT NULL,
     nome_treinador varchar(50) NOT NULL,
     localizacao varchar(12),
     insignias int,
@@ -26,13 +27,13 @@ CREATE TABLE Frutas(
 );
 
 CREATE TABLE Tipo(
-    tipo_id SMALLINT PRIMARY KEY,
+    tipo_id serial PRIMARY KEY,
     nome_tipo varchar(10) NOT NULL,
     CONSTRAINT sk_nome_tipo UNIQUE (nome_tipo)
 );
 
 CREATE TABLE Efeito(
-    efeito_id SMALLINT PRIMARY KEY,
+    efeito_id SERIAL PRIMARY KEY,
     nome_efeito varchar(10) NOT NULL,
     acuracia SMALLINT NOT NULL,
     dano SMALLINT,
@@ -41,17 +42,25 @@ CREATE TABLE Efeito(
 );
 
 CREATE TABLE Habilidade(
-    habilidade_id int PRIMARY KEY,
+    habilidade_id serial PRIMARY KEY,
     nome_habilidade varchar(20) NOT NULL,
     alcance char(1),
     dano SMALLINT,
-    tipo SMALLINT NOT NULL,
-    acuracia SMALLINT NOT NULL,
+    tipo int NOT NULL,
+    acuracia int NOT NULL,
     efeito SMALLINT,
     info varchar(150) NOT NULL,
     CONSTRAINT nome_habilidade_sk UNIQUE (nome_habilidade),
-    CONSTRAINT fk_tipo FOREIGN KEY (tipo) REFERENCES Tipo (tipo_id) ON DELETE RESTRICT ON UPDATE CASCADE,
-    CONSTRAINT fk_efeito FOREIGN KEY (efeito) REFERENCES Efeito (efeito_id) ON DELETE RESTRICT ON UPDATE CASCADE
+    CONSTRAINT fk_tipo FOREIGN KEY (tipo) REFERENCES Tipo (tipo_id) ON
+DELETE
+	RESTRICT ON
+	UPDATE
+		CASCADE,
+		CONSTRAINT fk_efeito FOREIGN KEY (efeito) REFERENCES Efeito (efeito_id) ON
+		DELETE
+			RESTRICT ON
+			UPDATE
+				CASCADE
 );
 
 CREATE TABLE TMs(
@@ -68,27 +77,63 @@ CREATE TABLE Pokebolas(
 
 CREATE TABLE Mochila(
     dono int NOT NULL,
-    nome_item varchar(50) NOT NULL,
+    nome_item varchar(60) NOT NULL,
     quantidade SMALLINT,
     origem varchar(15) NOT NULL,
     CONSTRAINT pk_mochila PRIMARY KEY (nome_item),
+    CONSTRAINT fk_item_chave FOREIGN KEY (nome_item) REFERENCES Itens_chave(nome_item),
+    CONSTRAINT fk_item_comum FOREIGN KEY (nome_item) REFERENCES Itens_comuns(nome_item),
+    CONSTRAINT fk_item_tms FOREIGN KEY (nome_item) REFERENCES TMs(nome_item),
+    CONSTRAINT fk_item_pokebolas FOREIGN KEY (nome_item) REFERENCES Pokebolas(nome_item),
+    CONSTRAINT fk_item_frutas FOREIGN KEY (nome_item) REFERENCES Frutas(nome_item),
     CONSTRAINT unq_dono_mochila UNIQUE (origem),
     CONSTRAINT fk_dono FOREIGN KEY (dono) REFERENCES Treinador (treinador_id)
 );
 
+CREATE TABLE Pokedex(
+	numero_pokedex int PRIMARY KEY,
+	nome_pokemon varchar(20),
+	tipo1 int NOT NULL,
+	tipo2 int,
+	som_emitido int,
+	regiao varchar(20),
+	info varchar(100),
+	pesquisa int,
+    CONSTRAINT fk_tipo1_pokedex FOREIGN KEY (tipo1) REFERENCES Tipo (tipo_id) ON
+DELETE
+	RESTRICT ON
+	UPDATE
+		CASCADE,
+		CONSTRAINT fk_tipo2_pokedex FOREIGN KEY (tipo2) REFERENCES Tipo (tipo_id) ON
+		DELETE
+			RESTRICT ON
+			UPDATE
+				CASCADE
+);
+
+CREATE TABLE Pokemons_Descobertos(
+	numero_pokemon int PRIMARY KEY,
+	treinador_id int,
+	
+	CONSTRAINT fk_numero_pokedex FOREIGN KEY(numero_pokemon) REFERENCES Pokedex(numero_pokedex),
+	CONSTRAINT fk_treinador_id FOREIGN KEY (treinador_id) REFERENCES Treinador(treinador_id)
+
+);
+
 CREATE TABLE Pokemon(
-    pokemon_id int PRIMARY KEY,
+    pokemon_id serial PRIMARY KEY,
     numero_pokedex int,
     nature varchar(10) NOT NULL,
-    nivel smallint NOT NULL,
-    hp smallint NOT NULL,
-    defesa smallint NOT NULL,
+    nivel SMALLINT NOT NULL,
+    hp SMALLINT NOT NULL,
+    defesa SMALLINT NOT NULL,
     habilidade1 varchar(20),
     habilidade2 varchar(20),
     habilidade3 varchar(20),
     habilidade4 varchar(20),
     treinador_id int,
     CONSTRAINT fk_treinador_id FOREIGN KEY (treinador_id) REFERENCES Treinador(treinador_id),
+    CONSTRAINT fk_num_pokedex FOREIGN KEY (numero_pokedex) REFERENCES Pokedex(numero_pokedex),
     CONSTRAINT fk_habilidade1 FOREIGN KEY (habilidade1) REFERENCES Habilidade(nome_habilidade),
     CONSTRAINT fk_habilidade2 FOREIGN KEY (habilidade2) REFERENCES Habilidade(nome_habilidade),
     CONSTRAINT fk_habilidade3 FOREIGN KEY (habilidade3) REFERENCES Habilidade(nome_habilidade),
@@ -98,13 +143,14 @@ CREATE TABLE Pokemon(
 CREATE TABLE Equipe(
     treinador_id int,
     pokemon int,
-    CONSTRAINT pk_equipe PRIMARY KEY(treinador_id, pokemon),
+    CONSTRAINT pk_equipe PRIMARY KEY(treinador_id,
+pokemon),
     CONSTRAINT fk_treinador_id FOREIGN KEY(treinador_id) REFERENCES Treinador(treinador_id),
     CONSTRAINT fk_pokemon FOREIGN KEY(pokemon) REFERENCES Pokemon(pokemon_id)
 );
 
 CREATE TABLE Batalha(
-    batalha_id int,
+    batalha_id serial,
     pokemon1 int,
     pokemon2 int,
     CONSTRAINT pk_batalha PRIMARY KEY(batalha_id),
@@ -121,7 +167,7 @@ CREATE TABLE Lugar(
 );
 
 CREATE TABLE Npc(
-    npc_id int PRIMARY KEY NOT NULL,
+    npc_id serial PRIMARY KEY NOT NULL,
     treinador int,
     localidade varchar(12) NOT NULL,
     sexo varchar(1) NOT NULL,
@@ -131,13 +177,8 @@ CREATE TABLE Npc(
     CONSTRAINT fk_localidade FOREIGN KEY(localidade) REFERENCES Lugar(localizacao)
 );
 
-CREATE TABLE Tipos(
-    tipo_id varchar(10) PRIMARY KEY,
-    nome_tipo varchar(10) NOT NULL
-);
-
 CREATE TABLE Loot(
-    loot_id int PRIMARY KEY,
+    loot_id serial PRIMARY KEY,
     nome_item varchar(60) NOT NULL,
     quantidade int NOT NULL,
     localidade varchar(12) NOT NULL,
@@ -147,34 +188,34 @@ CREATE TABLE Loot(
 CREATE TABLE Ginasio(
     nome_ginasio varchar(50) PRIMARY KEY,
     lider_id int NOT NULL,
-    tipo1 varchar(10) NOT NULL,
-    tipo2 varchar(10),
+    tipo1 int NOT NULL,
+    tipo2 int,
     cidade varchar(50) NOT NULL,
     loot int NOT NULL,
     CONSTRAINT fk_lider_id FOREIGN KEY(lider_id) REFERENCES Npc(npc_id),
-    CONSTRAINT fk_tipo1 FOREIGN KEY(tipo1) REFERENCES Tipos(tipo_id),
-    CONSTRAINT fk_tipo2 FOREIGN KEY(tipo2) REFERENCES Tipos(tipo_id),
+    CONSTRAINT fk_tipo1_gin FOREIGN KEY(tipo1) REFERENCES Tipo(tipo_id),
+    CONSTRAINT fk_tipo2_gin FOREIGN KEY(tipo2) REFERENCES Tipo(tipo_id),
     CONSTRAINT fk_cidade FOREIGN KEY(cidade) REFERENCES Lugar(localizacao),
     CONSTRAINT fk_loot FOREIGN KEY(loot) REFERENCES Loot(loot_id)
 );
 
-
 CREATE TABLE Batalhador(
 	npc_treinador_id int PRIMARY KEY,
-	tipo1 varchar(10) NOT NULL,
-	tipo2 varchar(10),
+	tipo1 int NOT NULL,
+	tipo2 int,
 	localidade varchar(12) NOT NULL,
 	loot int,
-
+	
+	
+	CONSTRAINT fk_npc_batalhador FOREIGN KEY(npc_treinador_id) REFERENCES Npc(npc_id),
 	CONSTRAINT fk_localidade FOREIGN KEY(localidade) REFERENCES Lugar(localizacao),
-	CONSTRAINT fk_tipo1 FOREIGN KEY(tipo1) REFERENCES Tipos(tipo_id),
-	CONSTRAINT fk_tipo2 FOREIGN KEY(tipo2) REFERENCES Tipos(tipo_id),
+	CONSTRAINT fk_tipo1_batalhador FOREIGN KEY(tipo1) REFERENCES Tipo(tipo_id),
+	CONSTRAINT fk_tipo2_batalhador FOREIGN KEY(tipo2) REFERENCES Tipo(tipo_id),
 	CONSTRAINT fk_loot FOREIGN KEY(loot) REFERENCES Loot(loot_id) 
 );
 
-
 CREATE TABLE PokeCenter(
-	pokecenter_id int PRIMARY KEY,
+	pokecenter_id serial PRIMARY KEY,
 	cura_disponivel int,
 	localidade varchar(12) NOT NULL,
 	info varchar(50),
@@ -182,18 +223,15 @@ CREATE TABLE PokeCenter(
 	CONSTRAINT fk_localidade FOREIGN KEY(localidade) REFERENCES Lugar(localizacao)
 );
 
-
-
 CREATE TABLE EnfermeiraJoy(
 	pokecenter_id int,
 	npc_id int,
 
-	CONSTRAINT pk_enfermeirajoy PRIMARY KEY(pokecenter_id, npc_id),
+	CONSTRAINT pk_enfermeirajoy PRIMARY KEY(pokecenter_id,
+npc_id),
 	CONSTRAINT fk_pokecenter_id FOREIGN KEY(pokecenter_id) REFERENCES PokeCenter(pokecenter_id),
 	CONSTRAINT fk_npc_id FOREIGN KEY(npc_id) REFERENCES Npc(npc_id)
 );
-
-
 
 CREATE TABLE PokeMart(
 	pokemart_id int PRIMARY KEY,
@@ -204,16 +242,15 @@ CREATE TABLE PokeMart(
 	CONSTRAINT fk_localidade FOREIGN KEY(localidade) REFERENCES Lugar(localizacao)
 );
 
-
 CREATE TABLE Lojista(
 	pokemart_id int ,
 	npc_id int,
 
-	CONSTRAINT pk_lojista PRIMARY KEY(pokemart_id, npc_id),
+	CONSTRAINT pk_lojista PRIMARY KEY(pokemart_id,
+npc_id),
 	CONSTRAINT fk_pokemart_id FOREIGN KEY(pokemart_id) REFERENCES PokeMart(pokemart_id),
 	CONSTRAINT fk_npc_id FOREIGN KEY(npc_id) REFERENCES Npc(npc_id)
 );
-
 
 CREATE TABLE Itens_Vendidos(
 	pokemart_id int ,
@@ -221,38 +258,10 @@ CREATE TABLE Itens_Vendidos(
 	quantidade int,
 	preco int,
 
-	CONSTRAINT pk_itens_vendidos PRIMARY KEY(pokemart_id, item_id),
+	CONSTRAINT pk_itens_vendidos PRIMARY KEY(pokemart_id,
+item_id),
 	CONSTRAINT fk_pokemart_id FOREIGN KEY(pokemart_id) REFERENCES PokeMart(pokemart_id),
 	CONSTRAINT fk_item_id FOREIGN KEY(item_id) REFERENCES Itens_Comuns(nome_item)
 );
 
-
-CREATE TABLE Pokemons_Descobertos(
-	numero_pokemon int PRIMARY KEY,
-	treinador_id int,
-	
-	
-	CONSTRAINT fk_treinador_id FOREIGN KEY (treinador_id) REFERENCES Treinador(treinador_id)
-
-);
-
-
-CREATE TABLE Pokedex(
-	numero_pokedex int PRIMARY KEY,
-	nome_pokemon varchar(20),
-	tipo1 varchar(10) NOT NULL,
-	tipo2 varchar(10),
-	som_emitido int,
-	regiao varchar(20),
-	info varchar(100),
-	pesquisa int,
-
-	CONSTRAINT fk_numero_pokedex FOREIGN KEY(numero_pokedex) REFERENCES Pokemons_Descobertos(numero_pokemon)
-);
-
 COMMIT;
-
-
-
-
-
